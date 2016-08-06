d09742c0455239a90de7bf680fdab934c3bfe8e7
Brandon <brandondecarlo@live.com>
2016-08-05 20:52:10 -0400

    Update club_hopper.rb

--- lib/club_hopper.rb
+++ lib/club_hopper.rb
@@ -6,8 +6,6 @@ require 'pry'
 # require_relative "club_hopper/version"
 
 require_relative './club_hopper/cli'
-require_relative './club_hopper/output'
-require_relative './club_hopper/cielo'
-# module ClubHopper
-#   # Your code goes here...
-# end
+
+require_relative './club_hopper/scraper'
+require_relative './club_hopper/event'
