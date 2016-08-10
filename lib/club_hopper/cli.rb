6455c26753bb271ed7f453b480c4d7eb702ff30f
Brandon <brandondecarlo@live.com>
2016-08-10 07:39:33 -0400

    cleaned up CLI controllerclass ClubHopper
     removed some old commented- count code

--- lib/club_hopper/cli.rb
+++ lib/club_hopper/cli.rb
@@ -3,9 +3,7 @@ class ClubHopper
    def call
      puts "Here's what's goin on at Ceilo!!!"
      ClubHopper::Scraper.new.scrape_events
-
-    list_events
-    
+     list_events
      menu
      goodbye  
    end
@@ -23,20 +21,12 @@ class ClubHopper
    def menu
      input = ""
      while input != "exit"
-       
         input = gets.strip 
 
         if input.to_i-1 <= ClubHopper::Event.all.size 
-        event = ClubHopper::Event.all[input.to_i-1]
+         event = ClubHopper::Event.all[input.to_i-1]
          puts "Event: #{event.name}"
          puts "URL: www.cieloclub.com#{event.url}"
-
-         puts "Would you like more info on this event? Y/N or 'exit' to exit."
-         answer = gets.strip
-
-         if ["Y", "YES"].include?(answer.upcase)
-         open_in_browser
-       
          elsif input == "exit"
          goodbye   
          else
@@ -45,14 +35,7 @@ class ClubHopper
         end   
       end
      end
-   end
 
-   def goodbye
-    puts "See you next time for more events at Cielo!!"
-    exit
-   end
-
-   end
 
    def goodbye
     puts "See you next time for more events at Cielo!!"
